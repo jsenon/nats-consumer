74ef187fddf23267cc3946fa6c9b1dcb2efca824
Julien SENON <julien.senon@gmail.com>
2018-05-11 04:35:12 +0200

    Update main with lint remarks

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"log"
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/nats-io/go-nats"
+	"go.uber.org/zap"
 )
 
 // NOTE: Use tls scheme for TLS, e.g. nats-sub -s tls://demo.nats.io:4443 foo
@@ -16,25 +18,56 @@ func printMsg(m *nats.Msg, i int) {
 
 func main() {
 
+	logger, err := zap.NewProduction()
+	if err != nil {
+		logger.Error("Failed to create zap logger",
+			zap.String("status", "ERROR"),
+			zap.Duration("backoff", time.Second),
+			zap.Error(err),
+		)
+	}
+
 	urls := os.Getenv("MY_NATSBOOTSTRAP")
 	showTime := os.Getenv("MY_TIMESTAMP")
 	subj := os.Getenv("MY_TOPIC")
 
 	nc, err := nats.Connect(urls)
 	if err != nil {
-		log.Fatalf("Can't connect: %v\n", err)
+		logger.Error("Error nats connection:",
+			zap.Error(err),
+			zap.String("status", "ERROR"),
+			zap.Duration("backoff", time.Second),
+		)
 	}
 
 	i := 0
 
-	nc.Subscribe(subj, func(msg *nats.Msg) {
-		i += 1
+	_, err = nc.Subscribe(subj, func(msg *nats.Msg) {
+		i++
 		printMsg(msg, i)
 	})
-	nc.Flush()
+	if err != nil {
+		logger.Error("Error nats subscription:",
+			zap.Error(err),
+			zap.String("status", "ERROR"),
+			zap.Duration("backoff", time.Second),
+		)
+	}
+	err = nc.Flush()
+	if err != nil {
+		logger.Error("Error nats flush:",
+			zap.Error(err),
+			zap.String("status", "ERROR"),
+			zap.Duration("backoff", time.Second),
+		)
+	}
 
 	if err := nc.LastError(); err != nil {
-		log.Fatal(err)
+		logger.Error("Error nats:",
+			zap.Error(err),
+			zap.String("status", "ERROR"),
+			zap.Duration("backoff", time.Second),
+		)
 	}
 
 	log.Printf("Listening on [%s]\n", subj)
